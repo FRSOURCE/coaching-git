8818070a96debb5a50d54c1a52da8836f3ea3278
Jakub Freisler <jakub@frsource.org>
2023-09-05 10:04:49 +0200

    feat: add button and console.log

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
   console.log('page has loaded!');
   document.querySelector('html').setAttribute('data-something', 'test');
+
+  document.querySelector('button').addEventListener('click', function () {
+    this.style.background = 'red';
+    console.log('something I forgot');
+  });
 });
